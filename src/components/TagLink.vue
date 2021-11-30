dda176b61651ce5b09465ee85fdc12c6d865e3e4
Ben Sayeg <bensayeg@gmail.com>
2021-11-30 16:26:53 -0500

    setting path to lower case

--- src/components/TagLink.vue
+++ src/components/TagLink.vue
@@ -3,7 +3,7 @@
     <ul class="thread--tags ">
       <li v-for="tag in tags" :key="tag.path" class="inline-flex m-2 p-2 border-gray-200 bg-gray-200">
         <g-link 
-          :to="'/tags/' + tag.path"
+          :to="'/tags/' + tag.path.toLowerCase()"
         >
           <span>{{ tag.title }}</span>
         </g-link>
